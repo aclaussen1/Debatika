9daa065e05dc64e3ee103154f0803d952b457aa2
aclaussen1 <aclaussen@ymail.com>
2015-09-05 15:00:52 -0400

    Added Debate1v1TabBarController (TabBarController) Class file, adjustments to storyboard for 1v1 debates

---
+++ Debatika/YouDebate/Debates1v1TabBarController.h
@@ -0,0 +1,13 @@
+//
+//  Debates1v1TabBarController.h
+//  Debatika
+//
+//  Created by Alexander Claussen on 9/5/15.
+//  Copyright (c) 2015 MudLord. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Debates1v1TabBarController : UITabBarController
+
+@end
