f5128dda0f51d12572e7e7c2003df84f60420e8c
aclaussen1 <aclaussen@ymail.com>
2015-09-05 15:42:22 -0400

    Fixed spacing between bar button items in the nav bar in the 1v1Debate tab bar controller

--- Debatika/YouDebate/Debates1v1TabBarController.m
+++ Debatika/YouDebate/Debates1v1TabBarController.m
@@ -18,18 +18,24 @@
     [super viewDidLoad];
 
     
-    //UIBarButtonItem *one = [[UIBarButtonItem alloc]initWithTitle:@"One" style:UIBarButtonItemStylePlain target:self action:@selector(homeButton:)];
+    /*
+     The following code creates 4 bar button items for this tab bar controller to add to the navigation bar. It spaces them out by finding the width of the screen. Funcitonally, it puts the the bar buttons that sit out on the outer edge, then puts a space equal to the width of the screen divided by 7. This is because there are essentially 7 things in the navigation bar (4 buttons and 3 spaces). Two arrays are used, one for the rightBarButtonItmes and one for the LeftBarButtonItems.
+     */
     UIBarButtonItem *one = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"HomeBarButtonItem"] style:UIBarButtonItemStylePlain target:self action:@selector(homeButton:)];
-    UIBarButtonItem *two = [[UIBarButtonItem alloc]initWithTitle:@"Two" style:UIBarButtonItemStylePlain target:self action:@selector(testMethod)];
-    UIBarButtonItem *three = [[UIBarButtonItem alloc]initWithTitle:@"Three" style:UIBarButtonItemStylePlain target:self action:@selector(testMethod)];
+    UIBarButtonItem *two = [[UIBarButtonItem alloc]initWithTitle:@"Two" style:UIBarButtonItemStylePlain target:self action:@selector(homeButton:)];
+    UIBarButtonItem *three = [[UIBarButtonItem alloc]initWithTitle:@"Three" style:UIBarButtonItemStylePlain target:self action:@selector(homeButton:)];
+    UIBarButtonItem *four = [[UIBarButtonItem alloc]initWithTitle:@"Four" style:UIBarButtonItemStylePlain target:self action:@selector(homeButton:)];
     
-    // create a spacer
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGSize screenSize = screenBound.size;
+    CGFloat screenWidth = screenSize.width;
     UIBarButtonItem *space = [[UIBarButtonItem alloc]initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:self action:nil];
-    space.width = 30;
+    space.width = screenWidth / 7;
     
-    NSArray *buttons = @[one, space, two, space, three];
-    
-    self.navigationItem.rightBarButtonItems = buttons;
+    NSArray *rightButtons = @[one, space, two];
+    NSArray *leftButtons = @[four, space, three];
+    self.navigationItem.rightBarButtonItems = rightButtons;
+    self.navigationItem.leftBarButtonItems = leftButtons;
 }
 
 - (IBAction)homeButton:(id)sender {
